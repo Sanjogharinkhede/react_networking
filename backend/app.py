20c89ebf38b8e5620354d525e2302ba63a084692
Sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-04-17 01:07:20 +0530

    Project is now on FastAPI No Flask

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,14 @@
-from flask import Flask, jsonify
-from flask_pymongo import PyMongo
-from flask_cors import CORS
+from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()  # Loads variables from .env
 
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})  
+app = FastAPI()
 
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
-mongo = PyMongo(app)
-
-@app.route('/')
+@app.get("/")
 def home():
-    return jsonify(message="Welcome to My app!")
+    return {"message": "We are starting with Fast API", "env": os.getenv("APP_ENV")}
 
-@app.route('/api/test', methods=['GET'])
-def test_route():
-    return jsonify(message="Backend is connected!")
-
-if __name__ == '__main__':
+if __name__ == '__main__':      
     app.run(debug=False, port=5000)
