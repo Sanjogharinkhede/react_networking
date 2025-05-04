b10b921672785296ad151b87e8025e5975a9dfa5
Sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-05-04 23:50:26 +0530

    user authentication

--- backend/routes/user_route.py
+++ backend/routes/user_route.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter,Body, Depends,HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from configs.db import get_db
 from schemas import User as UserRequest
 from services import user_service
@@ -14,3 +15,6 @@ db_depends=Annotated[Session,Depends(get_db)]
 async def create_new_user(db:db_depends,user:UserRequest = Body()):
         return user_service.create_user(db,user)
         
+@router.post("/login",status_code=status.HTTP_200_OK)
+async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],db:db_depends):
+        return user_service.get_token(form_data,db)
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -5,15 +5,32 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 from fastapi import Depends ,HTTPException
 from configs.db import get_db
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
 
 def create_user(db,user:UserRequest):
     try:
         user_model= User(**user.model_dump())
+        user_model.password=bcrypt_context.hash(user_model.password)
         db.add(user_model)
         db.commit()
         db.refresh(user_model)
-        return {"status":True,"msg":"User Added Successfully"}
+        return {"status":True,"msg":"User Added Successfully","user":user_model}
     except Exception as e:
         raise Exception("Something went wrong")
     
     
+def get_token(form_data,db):
+    is_a_user=authenticate_user(db,form_data.username,form_data.password)
+    if not is_a_user:
+        return {"status":False,"msg":"Failed Authentication", "form_data":form_data,"token":"token"}
+    return {"status":True, "form_data":form_data,"token":"token"}
+
+def authenticate_user(db,email, password)->bool:
+    user =db.query(User).filter(email==User.email).first()
+    if not user :
+        return False    
+    return bcrypt_context.verify(password,user.password)
