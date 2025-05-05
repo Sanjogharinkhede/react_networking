9ced110bd776058b7b9178c894c05d4071a4d8c0
Sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-05-06 00:57:03 +0530

    Added: decode JWT token

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,18 +1,20 @@
 from datetime import timedelta
+
+from starlette import status
+
 from schemas import User as UserRequest
 from models import User 
 
 from sqlalchemy.orm import Session
-from typing import Annotated
+from typing import Annotated, Any
 from fastapi import Depends ,HTTPException
 from configs.db import get_db
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from utils.auth_handler import create_access_token 
 
 
 bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
-
 def create_user(db,user:UserRequest):
     try:
         user_model= User(**user.model_dump())
@@ -22,18 +24,18 @@ def create_user(db,user:UserRequest):
         db.refresh(user_model)
         return {"status":True,"msg":"User Added Successfully","user":user_model}
     except Exception as e:
-        raise Exception("Something went wrong")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=e)
     
     
 def get_token(form_data,db):
     user:User=authenticate_user(db,form_data.username,form_data.password)
     if not user:
-        return {"status":False,"msg":"Failed Authentication"}
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="No user found with given credentials.")
     token=create_access_token(user.email,user.id,timedelta(minutes=30))
     return {"status":True, "form_data":form_data,"access_token":token,"token_type":"bearer"}
 
-def authenticate_user(db,email, password)->User:
+def authenticate_user(db,email, password)-> User| None:
     user =db.query(User).filter(email==User.email).first()
     if not user or not bcrypt_context.verify(password,user.password):
-        return None
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Password not matched for user")
     return user
--- backend/utils/auth_handler.py
+++ backend/utils/auth_handler.py
@@ -1,10 +1,16 @@
 
 from datetime import timedelta,datetime, timezone
 import os
+from typing import Annotated
+
 from dotenv import load_dotenv
-from jose import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from starlette import status
 
 load_dotenv()
+oauth2_bearer=OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def create_access_token(username:str, user_id:int, expire_delta:timedelta):
     encode={
@@ -13,4 +19,16 @@ def create_access_token(username:str, user_id:int, expire_delta:timedelta):
         'exp':datetime.now(timezone.utc)+expire_delta
     }
     
-    return jwt.encode(encode,os.getenv("SECRET_KEY"),algorithm=os.getenv("ALGORITHM")) 
+    return jwt.encode(encode,os.getenv("SECRET_KEY"),algorithm=os.getenv("ALGORITHM"))
+
+def get_user_with_jwt(token:Annotated[str,Depends(oauth2_bearer)]):
+    try:
+        payload =jwt.decode(token,os.getenv("SECRET_KEY"),algorithms=[os.getenv("ALGORITHM")])
+        email :str=payload.get('sub')
+        user_id:int =payload.get('id')
+        if not email or not user_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED ,detail="No User found")
+        return  {'email':email,"payload":payload}
+    except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED ,detail="JWT ERROR ")
+
