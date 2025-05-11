da16e0144d04162e854eb2455415a5a7dbd40860
Sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-05-12 01:33:08 +0530

    Added: Alembic for data migration

--- backend/configs/setting.py
+++ backend/configs/setting.py
@@ -16,7 +16,9 @@ class Settings(BaseSettings):
 settings = Settings()
 
 
-"""MONGO_URI=mongodb://localhost:27017/networkingdb
+"""
+
+MONGO_URI=mongodb://localhost:27017/networkingdb
 
 
 APP_ENV="Development"
@@ -28,5 +30,18 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 GEMINI_KEY=AIzaSyChn5MaunVyrExauHPstceTwaxtOtdTI14
 
 
-SQLALCHEMY_DATABASE_URL='sqlite:///./todos_app.db'
+SQLALCHEMY_DATABASE_URL_SQLITE='sqlite:///./todos_app.db'
+SQLALCHEMY_DATABASE_URL_POSTGRES='postgresql://postgres:Sanjog%40123@localhost:5432/sanjog_db'
+SQLALCHEMY_DATABASE_URL_MYSQL='mysql+pymysql://root:123456@localhost:3306/sanjog_may_db'
+
+HOST=0.0.0.0
+PORT=5000
+
+DB_USER=postgres
+DB_PASSWORD=Sanjog@123
+DB_HOST=localhost
+DB_PORT=5432
+DB_NAME=sanjog_db
+
+
 """
